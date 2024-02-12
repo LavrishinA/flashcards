b97a1fd8b1ea695998ae3b80c372651dc4ef4ded
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-02-12 13:11:33 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import App from '@/App'
 import ReactDOM from 'react-dom/client'
 
+import './styles/ index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
