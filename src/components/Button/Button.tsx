1541779547f5ae46e151d3e90c561b38ebc68fcd
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-02-22 00:48:03 +0300

    feat: add button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,29 @@
-import { ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
-type ButtonProps = {
-  as: ElementType
-  children: ReactNode
-}
+import { Typography } from '@/components/Typography'
 
-export const Button = (props: ButtonProps) => {
-  const { as: Component = 'button', children } = props
+import s from './Button.module.scss'
 
-  return <Component>{children}</Component>
+type ButtonProps<T extends ElementType> = {
+  as?: T
+  fullWidth?: boolean
+  icon?: ReactNode
+  label?: string
+  type?: 'primary' | 'secondary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const { as: Component = 'button', className, fullWidth, icon, label, type = 'primary' } = props
+
+  return (
+    <Component
+      className={`${s.btn} ${className} ${s[type]} ${fullWidth && s.fullWidth}`}
+      {...props}
+    >
+      {icon}
+      <Typography as={'span'} variant={'subtitle2'}>
+        {label}
+      </Typography>
+    </Component>
+  )
 }
