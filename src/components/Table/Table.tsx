d16cb5d4c3125706df26cb59562e1981812debd4
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-03-03 22:23:28 +0300

    init: start dev table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,3 +1,33 @@
-export const Table = () => {
-  return <div></div>
-}
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+const Table = forwardRef<HTMLTableElement, ComponentPropsWithoutRef<'table'>>((props, ref) => {
+  return <table {...props} ref={ref}></table>
+})
+
+const TableBody = forwardRef<HTMLTableSectionElement, ComponentPropsWithoutRef<'tbody'>>(
+  (props, ref) => {
+    return <tbody {...props} ref={ref} />
+  }
+)
+
+const TableHeader = forwardRef<HTMLTableSectionElement, ComponentPropsWithoutRef<'thead'>>(
+  (props, ref) => {
+    return <thead {...props} ref={ref}></thead>
+  }
+)
+
+const TableRow = forwardRef<HTMLTableRowElement, ComponentPropsWithoutRef<'tr'>>((props, ref) => {
+  return <tr {...props} ref={ref} />
+})
+
+const TableCellHead = forwardRef<HTMLTableCellElement, ComponentPropsWithoutRef<'th'>>(
+  (props, ref) => {
+    return <th {...props} ref={ref} />
+  }
+)
+
+const TableCell = forwardRef<HTMLTableCellElement, ComponentPropsWithoutRef<'td'>>((props, ref) => {
+  return <td {...props} ref={ref} />
+})
+
+export { Table, TableBody, TableCell, TableCellHead, TableHeader, TableRow }
