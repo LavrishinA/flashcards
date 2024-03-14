9891a229d69df8a48d1243e6e27cac88e6f4b127
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-03-14 12:44:03 +0300

    ref: add text type for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
 import { Typography } from '@/components/Typography'
+import { clsx } from 'clsx'
 
 import s from './Button.module.scss'
 
@@ -9,21 +10,20 @@ type ButtonProps<T extends ElementType> = {
   fullWidth?: boolean
   icon?: ReactNode
   label?: string
-  type?: 'primary' | 'secondary'
+  type?: 'primary' | 'secondary' | 'text'
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
   const { as: Component = 'button', className, fullWidth, icon, label, type = 'primary' } = props
 
   return (
-    <Component
-      className={`${s.btn} ${className} ${s[type]} ${fullWidth && s.fullWidth}`}
-      {...props}
-    >
+    <Component className={clsx(s.btn, className, s[type], fullWidth && s.fullWidth)} {...props}>
       {icon}
-      <Typography as={'span'} variant={'subtitle2'}>
-        {label}
-      </Typography>
+      {
+        <Typography as={'span'} variant={type === 'text' ? 'caption' : 'subtitle2'}>
+          {label}
+        </Typography>
+      }
     </Component>
   )
 }
