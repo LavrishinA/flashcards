0cde1bf17365a4d248186f51ef08d7cea6805fc3
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-03-20 14:03:12 +0300

    feat:  add cover block for deck

--- src/widgets/deck-list/DeckList.tsx
+++ src/widgets/deck-list/DeckList.tsx
@@ -2,6 +2,9 @@ import { DeckItem } from '@/entities/decks'
 import { dateFormater } from '@/shared/lib/dateFormater'
 import { Table } from '@/shared/ui/Table'
 import { Typography } from '@/shared/ui/Typography'
+import { AspectRatio } from '@radix-ui/react-aspect-ratio'
+
+import s from './DeckList.module.scss'
 
 type Props = {
   decks: DeckItem[]
@@ -27,7 +30,16 @@ export const DeckList = (props: Props) => {
           {decks &&
             decks.map(d => (
               <Table.TableRow key={d.id}>
-                <Table.TableCell>{d.name}</Table.TableCell>
+                <Table.TableCell className={s.name}>
+                  {d.cover && (
+                    <div className={s.ratioContainer}>
+                      <AspectRatio ratio={21 / 9}>
+                        <img alt={''} className={s.image} src={d?.cover} />
+                      </AspectRatio>
+                    </div>
+                  )}
+                  {d.name}
+                </Table.TableCell>
                 <Table.TableCell>{d.cardsCount}</Table.TableCell>
                 <Table.TableCell>{dateFormater(d.updated)}</Table.TableCell>
                 <Table.TableCell>{dateFormater(d.created)}</Table.TableCell>
