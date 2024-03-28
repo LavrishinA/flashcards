9d437c83301f98903c3bdc7264494e085e05da28
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-03-29 01:23:46 +0300

    feat: add change profile component

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,2 +1,7 @@
-export { useLoginMutation, useLogoutMutation, useMeQuery } from './api/userApi'
+export {
+  useLoginMutation,
+  useLogoutMutation,
+  useMeQuery,
+  useUpdateProfileMutation,
+} from './api/userApi'
 export type { userLoginPayload } from './model/types'
--- src/features/edit-profile/ui/EditProfile.tsx
+++ src/features/edit-profile/ui/EditProfile.tsx
@@ -1,18 +1,23 @@
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
-import { useLogoutMutation, useMeQuery } from '@/entities/user'
+import { useLogoutMutation, useMeQuery, useUpdateProfileMutation } from '@/entities/user'
 import { Avatar } from '@/shared/ui/Avatar'
 import { Button } from '@/shared/ui/Button'
 import { Card } from '@/shared/ui/Card'
 import { Typography } from '@/shared/ui/Typography'
-import { LogoutIcon } from '@/shared/ui/icons'
+import { ControlledInput } from '@/shared/ui/controlled/controlled-input/Controlled-input'
+import { EditIcon, LogoutIcon } from '@/shared/ui/icons'
 
 import s from './EditProfile.module.scss'
 
 export const EditProfile = () => {
   const { data: user } = useMeQuery()
   const [logout] = useLogoutMutation()
+  const [updateProfile] = useUpdateProfileMutation()
   const navigate = useNavigate()
+  const [isEdit, setIsEdit] = useState(false)
 
   const logoutHandler = () => {
     logout()
@@ -20,21 +25,89 @@ export const EditProfile = () => {
       .then(() => {
         navigate('/sign-in')
       })
+      .finally(() => reset())
   }
+
+  const {
+    control,
+    formState: { dirtyFields },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm({
+    defaultValues: {
+      avatar: user?.avatar,
+      name: user?.name,
+    },
+  })
+
+  const submitForm = (data: any) => {
+    const formData = new FormData()
+
+    formData.append('avatar', data.avatar?.[0])
+    formData.append('name', data.name)
+    updateProfile(formData)
+      .unwrap()
+      .finally(() => {
+        setIsEdit(false)
+      })
+  }
+
+  useEffect(() => {
+    reset({
+      avatar: user?.avatar,
+      name: user?.name,
+    })
+  }, [reset, user?.name, user?.avatar])
 
   return (
     <>
       <Card className={s.card}>
         <Typography className={s.title} variant={'h1'}>
           Personal information
-          {user && <Avatar className={s.avatar} src={user?.avatar} username={user?.name} />}
         </Typography>
-        <Button
-          icon={<LogoutIcon height={16} width={16} />}
-          label={'Log out'}
-          onClick={logoutHandler}
-          variant={'secondary'}
-        />
+
+        <form onSubmit={handleSubmit(submitForm)}>
+          <div>
+            {user && <Avatar className={s.avatar} src={user.avatar} username={user?.name} />}
+            <label htmlFor={'avatar'}>
+              <EditIcon height={16} width={16} />
+              <input multiple={false} {...register('avatar')} id={'avatar'} type={'file'} />
+            </label>
+          </div>
+          {isEdit ? (
+            <ControlledInput
+              autoFocus
+              control={control}
+              label={'Name'}
+              name={'name'}
+              onBlur={() => {
+                setIsEdit(false)
+              }}
+            />
+          ) : (
+            <div style={{ display: 'flex' }}>
+              <Typography variant={'h2'}>{user?.name}</Typography>
+              <Button
+                icon={<EditIcon height={16} width={16} />}
+                onClick={() => setIsEdit(!isEdit)}
+                type={'button'}
+                variant={'text'}
+              />
+            </div>
+          )}
+          {dirtyFields.avatar || dirtyFields.name ? (
+            <Button label={'Save changes'} variant={'primary'} />
+          ) : (
+            <Button
+              icon={<LogoutIcon height={16} width={16} />}
+              label={'Logout'}
+              onClick={logoutHandler}
+              type={'button'}
+              variant={'secondary'}
+            />
+          )}
+        </form>
       </Card>
     </>
   )
