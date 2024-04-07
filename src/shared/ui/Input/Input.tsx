7a34f725314dfe42c238c6fe72bac6fca1a78606
AnatolyNikol <fanka4ectba@mail.ru>
2024-04-07 15:20:58 +0500

    feat: add search type and clearInput to SearchCards

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -16,7 +16,6 @@ export type InputProps = {
   errorMessage?: string
   label?: string
   onInputClear?: (value: string) => void
-  // onValueChange?: (value: string) => void
   search?: boolean
 } & ComponentPropsWithoutRef<'input'>
 
@@ -29,7 +28,6 @@ export const Input = forwardRef<ElementRef<'input'>, InputProps>(
       label,
       onChange,
       onInputClear,
-      // onValueChange,
       search,
       type,
       value,
@@ -44,7 +42,6 @@ export const Input = forwardRef<ElementRef<'input'>, InputProps>(
 
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
       onChange?.(e)
-      // onValueChange && onValueChange(e.currentTarget.value)
     }
 
     const onClearClickHandler = () => {
