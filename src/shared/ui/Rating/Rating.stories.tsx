bcf316ce84ced5be99f32c78a8f6faeb9c15255b
VyacheslavSavchik <rothmans.830@gmail.com>
2024-04-24 15:28:13 +0300

    fix: cards-list

--- src/shared/ui/Rating/Rating.stories.tsx
+++ src/shared/ui/Rating/Rating.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+
 import { Rating } from '@/shared/ui/Rating/Rating'
 
 const meta: Meta<typeof Rating> = {
@@ -14,11 +16,21 @@ type Story = StoryObj<typeof meta>
 export const FullRating: Story = {
   args: {
     maxStar: 5,
-    rating: 3,
     size: 16,
   },
-  render: ({ color, maxStar, rating, size }) => {
-    return <Rating color={color} maxStar={maxStar} rating={rating} size={size} />
+  render: ({ color, maxStar, size }) => {
+    const [rating, setRating] = useState<number>(2)
+    const changeRatingHandler = (r: number) => setRating(r)
+
+    return (
+      <Rating
+        color={color}
+        maxStar={maxStar}
+        onClick={changeRatingHandler}
+        rating={rating}
+        size={size}
+      />
+    )
   },
 }
 
