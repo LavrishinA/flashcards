aa1c0c430d28b0101ee09ea0d14cb99a42634a41
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-05-09 01:51:10 +0300

    ref: zod file validation extract into separate file

--- src/features/decks/deck-form/model/deck-form-zod-schema.ts
+++ src/features/decks/deck-form/model/deck-form-zod-schema.ts
@@ -1,26 +1,9 @@
+import { fileValidation } from '@/shared/lib/zodFileValidation'
 import * as z from 'zod'
-
-const ACCEPTED_IMAGE_MIME_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
-const MAX_FILE_SIZE = 1024 * 1024 * 5
 
 export const DeckFormZodSchema = z
   .object({
-    cover: z
-      .optional(z.any())
-      .refine(file => {
-        if (!file || !file[0]) {
-          return true
-        }
-
-        return file[0].size <= MAX_FILE_SIZE
-      }, `Max image size is 5MB.`)
-      .refine(file => {
-        if (!file || !file[0]) {
-          return true
-        }
-
-        return ACCEPTED_IMAGE_MIME_TYPES.includes(file[0].type)
-      }, 'Only .jpg, .jpeg, .png and .webp formats are supported.'),
+    cover: fileValidation,
     isPrivate: z.boolean().default(false),
     name: z.string(),
   })
