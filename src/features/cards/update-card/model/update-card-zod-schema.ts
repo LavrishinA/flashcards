fbeba9c8976afe84df142ac429392991c0ef98cf
AnatolyNikol <fanka4ectba@mail.ru>
2024-05-14 19:03:14 +0500

    refactor: delete UpdateCardForm

--- src/features/cards/update-card/model/update-card-zod-schema.ts
+++ src/features/cards/update-card/model/update-card-zod-schema.ts
@@ -1,6 +1,6 @@
-import { z } from 'zod'
-
-export const updateCardZodSchema = z.object({
-  answer: z.string().min(5),
-  question: z.string().min(5),
-})
+// import { z } from 'zod'
+//
+// export const updateCardZodSchema = z.object({
+//   answer: z.string().min(5),
+//   question: z.string().min(5),
+// })
